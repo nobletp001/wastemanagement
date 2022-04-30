eb7b8b51b83ddf1f49422574013c66164697682c
dchief1 <igwesiemmanuel4@gmail.com>
2022-05-01 00:10:54 +0100

    added tailwind and created a view folder for the links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 import Footer from '../components/Footer/Footer'
+import About from '../views/About/about'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
 
     </div>
+    <About/>
    <Footer/>
   </>
 }
