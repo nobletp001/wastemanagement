e0a8e4821914ed6a25453f0776989b52f05f49a3
dchief1 <igwesiemmanuel4@gmail.com>
2022-05-01 12:27:15 +0100

    moved the about page to the pages folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 import Footer from '../components/Footer/Footer'
-import About from '../views/About/about'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
@@ -11,7 +10,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
 
     </div>
-    <About/>
    <Footer/>
   </>
 }
---
+++ pages/about.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const About: NextPage = () => {
+  return (
+    <div >
+      <Head>
+        <title>Waste Management</title>
+        <meta name="description" content="waste management" />
+        <link rel="icon" href="" />
+      </Head>
+
+     
+    </div>
+  )
+}
+
+export default About
