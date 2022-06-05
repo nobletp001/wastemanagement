146b7eeb534c96d343a1c89f2ff37568e79e809c
dchief1 <igwesiemmanuel4@gmail.com>
2022-06-05 14:11:01 +0100

    about and contact page completed for laptop view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 import Footer from '../components/Footer/Footer'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps,  }: AppProps) {
 
