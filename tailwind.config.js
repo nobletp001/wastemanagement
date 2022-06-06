0739ec9ece61d2908b643d67d4b6ce3824d344cb
dchief1 <igwesiemmanuel4@gmail.com>
2022-06-06 13:37:47 +0100

    problem fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,45 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'hero' : ' #3CAA35'
-    },
-    fontFamily: {
-      'lato': [' Lato, system-ui, sans-serif']
-    },
+    
     extend: {
       height: {
-        'abouth': '2700px',
-        'texth': '63px',
+        'abouth': '2800px', 
+        'contacth': '1200px', 
+        'texth': '43px',
+        'imgh': '119px',
+        'imgw': '500px',
+        'herop': '645px',
         'ph': '276px',
+        'ch': '980px',
+        'boxh': '723px',
+        'formh': '923px',
+        'teamh': '799px',
+      },
+      colors: {
+        'hero' : ' #3CAA35',
+        'lhero' : ' #a7daa3cc',
+        'brd' : ' #B4BEC8',
+        'team' : ' #EBF7EB',
+      },
+      boxShadow: {
+        'formshade' : '2px 10px 28px rgba(75, 0, 129, 0.12)',
+      },
+      fontFamily: {
+        'lato': [' Lato, system-ui, sans-serif'],
+        'poppins': [' Poppins, sans-serif']
       },
       width: {
         'aboutw': '1920px',
-        'textw': '323px',
+        'abouthw': '1098px',
+        'textw': '523px',
         'pw': '708px',
+        'formw': '432px',
+      },
+      margin: {
+        'lg': '25rem',
+        'ad': '21rem',
+        'abt': '744px',
       },
       left: {
         'textl': '945px',
